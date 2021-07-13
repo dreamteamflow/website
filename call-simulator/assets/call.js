91339298f5033c110eed26bb8959ec5998682173
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2021-07-13 12:40:18 +0200

    Add transition effect

--- call-simulator/assets/call.js
+++ call-simulator/assets/call.js
@@ -1,4 +1,6 @@
+// Wait until the document (DOM) is fully loaded.
 $(document).ready(function() {
+  // Load the history data...
   $.getJSON("data/history.json").done(data => {
     $.each(data, (index, item) => {
       //
@@ -6,4 +8,14 @@ $(document).ready(function() {
   }).fail(error => {
     //
   });
+
+  // Active the input when search icon clicked.
+  $(".search-container .search-btn").click(() => {
+    $(".search-input input").focus();
+  });
+
+  // Input section transition on focus.
+  $(".search-input input")
+    .focus(() => { $(".header").addClass("input-focused"); })
+    .focusout(() => { $(".header").removeClass("input-focused"); });
 });
